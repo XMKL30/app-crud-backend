cf2bc33745cc2fa5c7e09f60291c6cb55ea52cc7
XMKL30 <mikjaeljeandet_2003@hotmail.com>
2023-12-13 20:19:14 -0500

    Creación del proyecto backend

---
+++ src/main/java/com/mikjael/appcrudback/AppCrudBackApplication.java
@@ -0,0 +1,13 @@
+package com.mikjael.appcrudback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCrudBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCrudBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikjael/appcrudback/AppCrudBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikjael.appcrudback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCrudBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
